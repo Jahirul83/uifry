046367e6965f3c2383c71ea3c21d08de6cc26c9c
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-04 00:51:47 +0600

    navBar and footer added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "../../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
+
+
+const Home = () => {
+    return (
+        <>
+            <NavBar></NavBar>
+            <Footer></Footer>
+        </>
+    );
+};
+
+export default Home;
