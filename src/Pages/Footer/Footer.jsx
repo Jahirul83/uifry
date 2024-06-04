7c2a00b3cab9eab751c37d0a2000b8bb31286173
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-04 22:22:43 +0600

    footer and title edit

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -5,46 +5,41 @@ const Footer = () => {
         <div>
             <footer className="footer p-10 bg-base-200 text-base-content">
                 <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <h6 className="text-3xl font-bold">Services</h6>
+                    <a className="link link-hover font-bold">Branding</a>
+                    <a className="link link-hover font-bold">Design</a>
+                    <a className="link link-hover font-bold">Marketing</a>
+                    <a className="link link-hover font-bold">Advertisement</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <h6 className="text-3xl font-bold">Links</h6>
+                    <a className="link link-hover font-bold">Home</a>
+                    <a className="link link-hover font-bold">About us</a>
+                    <a className="link link-hover font-bold">bookings</a>
+                    <a className="link link-hover font-bold">blog</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <h6 className="text-3xl font-bold">Legal</h6>
+                    <a className="link link-hover font-bold">Terms Of Use</a>
+                    <a className="link link-hover font-bold">Privacy Policy</a>
+                    <a className="link link-hover font-bold">Jobs</a>
+                    <a className="link link-hover font-bold">Cookie Policy</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <h6 className="text-3xl font-bold">Product</h6>
+                    <a className="link link-hover font-bold">Take Tour</a>
+                    <a className="link link-hover font-bold">Live Chat</a>
+                    <a className="link link-hover font-bold">Reveiws</a>
                 </nav>
-                <form>
-                    <h6 className="footer-title">Newsletter</h6>
-                    <fieldset className="form-control w-80">
-                        <label className="label">
-                            <span className="label-text">Enter your email address</span>
-                        </label>
-                        <div className="join">
-                            <input type="text" placeholder="user@site.com" className="input input-bordered join-item" />
-                            <button className="btn btn-primary join-item">Subscribe</button>
-                        </div>
-                    </fieldset>
-                </form>
+                <nav>
+                    <h6 className="text-3xl font-bold">Newsletter</h6>
+                    <div>
+                        <input type="text" className="input input-bordered" placeholder="Your email" />
+                        <button className="btn bg-black text-white">Subscribe</button>
+                    </div>
+                </nav>
             </footer>
-            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content border-t-4 border-zinc-700	">
                 <aside>
                     <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
                 </aside>
